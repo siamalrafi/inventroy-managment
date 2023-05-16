5bffb848d29adeec790ec3c199bae06c52d1fec0
siamalrafi <rahathasandipto@gmail.com>
2023-05-16 18:27:41 +0600

    This is the file uploading system

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -197,7 +197,6 @@ exports.updateMany = async (req, res, next) => {
 
 exports.fileUpload = async (req, res) => {
    try {
-      console.log(req.body);
       res.status(200).json(req.files);
    } catch (error) {}
 };
