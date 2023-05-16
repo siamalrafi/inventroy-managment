5c965c0948123edffacf4e78566c372f026fa86d
siamalrafi <rahathasandipto@gmail.com>
2023-05-16 18:46:13 +0600

    new data added. that is very important

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const productRoutes = require("./routes/v1/products.route");
 const brandRoute = require("./routes/v1/brand.route");
 const categoryRoute = require("./routes/v1/category.route");
 const storeRoute = require("./routes/v1/store.route");
+const supplierRoute = require("./routes/v1/supplier.route");
+const stockRoute = require("./routes/v1/stock.route");
+const userRoute = require("./routes/v1/user.route");
+
 
 app.use(express.json());
 app.use(cors());
@@ -14,6 +18,9 @@ app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/brand", brandRoute);
 app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/store", storeRoute);
+app.use("/api/v1/supplier", supplierRoute);
+app.use("/api/v1/stock", stockRoute);
+app.use("/api/v1/user", userRoute);
 
 // get main routes ---
 app.get("/", (req, res) => {
---
+++ routes/v1/supplier.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const supplierController = require("../../controller/supplier.controller");
+const router = express.Router();
+
+router
+   .route("/")
+   // post supplier
+   .post(supplierController.createSupplier)
+   .get(supplierController.getSuppliers);
+
+router.route("/:id").get(supplierController.getSupplierById).patch(supplierController.updateSupplier);
+
+module.exports = router;
