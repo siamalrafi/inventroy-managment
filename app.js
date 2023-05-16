487198bb5e477fc7464b2c4976180aeca9196900
siamalrafi <rahathasandipto@gmail.com>
2023-05-16 17:27:24 +0600

    category

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const app = express();
 const cors = require("cors");
 const productRoutes = require("./routes/v1/products.route");
 const brandRoute = require("./routes/v1/brand.route");
+const categoryRoute = require("./routes/v1/category.route")
+
 
 app.use(express.json());
 app.use(cors());
 
+
 // use product routes ---
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/brand", brandRoute);
+app.use("/api/v1/category", categoryRoute);
+
+
+
 // get main routes ---
 app.get("/", (req, res) => {
    res.send("Route is working! YaY!");
