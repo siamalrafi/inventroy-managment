94ad4bf8546521a01578c570476a44bef688c426
siamalrafi <rahathasandipto@gmail.com>
2023-06-01 16:59:36 +0600

    create a new category schema object

--- model/Category.js
+++ model/Category.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
+// create a new category schema object
 const categorySchema = mongoose.Schema(
    {
       name: {
