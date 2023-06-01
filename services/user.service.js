b2cae8491fd77d0691c80789da8cca24a1bc724f
siamalrafi <rahathasandipto@gmail.com>
2023-06-01 17:11:56 +0600

    sign up a service

--- services/user.service.js
+++ services/user.service.js
@@ -1,12 +1,13 @@
 const User = require("../model/User");
 
+// sign up a service
 exports.signupService = async (userInfo) => {
    const user = await User.create(userInfo);
    return user;
 };
 
 exports.findUserByEmail = async (email) => {
-  return await User.findOne({ email });
+   return await User.findOne({ email });
 };
 
 // exports.findUserByToken = async (token) => {
