2dff104767f5ff97bfae3969b7507d9184020203
siamalrafi <rahathasandipto@gmail.com>
2023-06-01 17:06:00 +0600

    get all categories

--- services/category.service.js
+++ services/category.service.js
@@ -1,5 +1,6 @@
 const Category = require("../model/Category");
 
+// get all categories
 exports.getCategoriesService = async () => {
    const categories = await Category.find({});
    return categories;
