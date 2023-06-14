3fbb9f6011857c062b0e7d2dbfcf660b4dea24c0
siamalrafi <rahathasandipto@gmail.com>
2023-06-14 17:09:48 +0600

    new

--- routes/v1/supplier.route.js
+++ routes/v1/supplier.route.js
@@ -3,13 +3,13 @@ const supplierController = require("../../controller/supplier.controller");
 const router = express.Router();
 
 router
-
    .route("/")
    // post supplier
    .post(supplierController.createSupplier)
    .get(supplierController.getSuppliers);
 
 router.route("/:id").get(supplierController.getSupplierById).patch(supplierController.updateSupplier);
+
 
 // supplier routes
 module.exports = router;
